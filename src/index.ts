8ab0ddfc72e65fc459e265468c71dd66100d024b
Samuel Chen <sche842@gmail.com>
2024-10-15 22:46:39 +1100

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world!!!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(compression()); // compresses all responses
+app.use(cookieParser());
+app.use(bodyParser.json()); // to parse JSON-formatted request bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to parse URL-encoded data (from forms)
+
+const server = http.createServer(app);
