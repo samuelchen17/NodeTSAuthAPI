b60a993eb779895c68c96dfdb699c988f8861c60
Samuel Chen <sche842@gmail.com>
2024-10-16 18:24:23 +1100

    error handling

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import { createUser, getUserByEmail } from "../services/user.services";
+import { random, authentication } from "../utils/user.utils";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || username) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
