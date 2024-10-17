0ece1ab0eda8ea3bff66b1fa860c0a6f6d61e725
Samuel Chen <sche842@gmail.com>
2024-10-17 22:00:36 +1100

    completed

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import {
   deleteUserById,
   getUserByEmail,
+  getUserById,
   getUsers,
 } from "../services/user.services";
 import { CustomError } from "../utils/errorHandler.utils";
@@ -31,11 +32,39 @@ export const deleteUser = async (
     const deletedUser = await deleteUserById(id);
 
     if (!deletedUser) {
-      res.status(404).json({ message: "User does not exist" });
+      next(new CustomError(404, "User does not exist"));
     }
 
     res.status(200).json({ message: "User deleted", deletedUser: deletedUser });
   } catch (error) {
-    next(new CustomError(400, "Unable to to delete user"));
+    next(new CustomError(400, "Unable to delete user"));
+  }
+};
+
+export const updateUsername = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      next(new CustomError(404, "User does not exist"));
+    }
+
+    const user = await getUserById(id);
+
+    if (!user) {
+      next(new CustomError(404, "User does not exist"));
+    }
+
+    user!.username = username;
+    await user!.save();
+
+    res.status(200).json({ message: "Username updated", user });
+  } catch (error) {
+    next(new CustomError(400, "Unable to update details"));
   }
 };
